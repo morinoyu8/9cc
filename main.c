456ac8ad2c3d8f4c4b76ffa26958588b6e889d2c
山本航平 <kohei0713@MacBook-ProM.local>
2022-09-10 02:51:20 +0900

    support file input

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 #include "9cc.h"
 
 // 現在着目しているトークン
@@ -9,6 +12,31 @@ char *user_input;
 
 Node *code[100];
 
+char *read_file(char *path) {
+    // ファイルを開く
+    FILE *fp = fopen(path, "r");
+    if (!fp)
+        error("cannot open %s: %s", path, strerror(errno));
+    
+    // ファイルの長さを調べる
+    if (fseek(fp, 0, SEEK_END) == -1)
+        error("%s: fseek: %s", path, strerror(errno));
+    size_t size = ftell(fp);
+    if (fseek(fp, 0, SEEK_SET) == -1)
+        error("%s: fseek: %s", path, strerror(errno));
+
+    // ファイル内容を読み込む
+    char *buf = calloc(1, size + 2);
+    fread(buf, size, 1, fp);
+
+    //ファイルが必ず"\n\0"で終わっているようにする
+    if (size == 0 || buf[size - 1] != '\n')
+        buf[size++] = '\n';
+    buf[size] = '\0';
+    fclose(fp);
+    return buf;
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         error("引数の個数が正しくありません");
@@ -16,8 +44,8 @@ int main(int argc, char **argv) {
     }
 
     // トークナイズしてパースする
-    user_input = argv[1];
-    token = tokenize(argv[1]);
+    user_input = read_file(argv[1]);
+    token = tokenize(user_input);
     program();
 
     // アセンブリの前半部分
